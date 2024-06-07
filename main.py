88ad5824934231aafdfbc2b57b6d6f1b9807c942
Akshay <akshayblueh@gmail.com>
2024-06-07 13:01:17 +0530

    first_func_added

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+from typing import List
+
+class calculation:
+    def __init__(self,input_array:List):
+        self.inputs = input_array
+    
+    def get_sum(self):
+        return np.sum(self.inputs)
+
+calc = calculation([3,4,5])
+print(calc.get_sum())
